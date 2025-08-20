f9cf4e87d7ed80f80c5567aaf7e817994d54a844
Đỗ Kim Nhường <127850790+KimNhuong@users.noreply.github.com>
2025-08-20 15:24:47 +0700

    small adjustment

--- Frontend/sync-space-frontend/src/pages/AboutPageModule/EducationBgPage.js
+++ Frontend/sync-space-frontend/src/pages/AboutPageModule/EducationBgPage.js
@@ -2,14 +2,14 @@ function EducationPage() {
   return (
     <div className="bg-indigo-600 w-full h-full fixed grid grid-cols-3 grid-rows-3">
       <div classNaame="w-full h-full grid grid-cols-2 row-span-2 col-span-2">
-        <img src="/retroCamera.png" alt="retroCAm" className="rounded-lg w-full h-full ml-24 z-10 "/>
-        <img src="/Mypic2.jpg" alt="mypic2" className=" relative rounded-lg w-2/4 h-2/4 mt-24 right-28 shadow-xl shadow-indigo-50 ring-2 rotate-6 "/>
+        <img src="/retroCamera.png" alt="retroCAm" className="absolute grid rounded-lg w-full h-full ml-24 z-10 right-[30rem]"/>
+        <img src="/Mypic2.jpg" alt="mypic2" className="w-full rounded-lg h-full ml-[20rem] mt-24 shadow-xl shadow-indigo-50 ring-2 rotate-6 "/>
       </div>
-      <div className="grid items-center relative right-28">
+      <div className="grid col-start-3 items-center relative right-28"> 
         <p className="border-4 rounded-2xl w-fit p-2 font-spartan font-bold text-white absolute text-4xl">2019-2022</p>
         <p className="font-spartan font-bold text-white mt-44  text-4xl  "> BAC KAN HIGHSCHOOL FOR THE GIFTED</p>
         </div>
-      <div className="grid items-center relative right-28">
+      <div className="grid items-center col-start-3 relative right-28">
         <p className="border-4 rounded-2xl w-fit p-2 font-spartan font-bold text-white absolute text-4xl">2022-2026</p>
         <p className="font-spartan font-bold text-white mt-44  text-4xl  "> HANOI UNIVERSITY OF SCIENCE AND TECHNOLOGY</p>
         </div>
